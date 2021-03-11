6685dd6816f0874989c066d6d87733cfcb12b115
Sabrina Jones <sabrina.jones87@gmail.com>
2021-03-11 03:09:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(record){
+  let win = record.find( ({ result}) => result === "W");
+  if (win){
+    return win.year
+  } else {
+    return undefined
+  }
 }
